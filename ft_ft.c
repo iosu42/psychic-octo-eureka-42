0c2e16d6bd5fce2e6f9a8980b7589d95d63d9753
iosu42 <josuabasolo42@gmail.com>
2022-10-29 22:02:51 +0100

    ex01

---
+++ ft_ft.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+void ft_ft(int *nbr)
+{
+    *nbr = 42;
+}
+
+int main()
+{
+    int i = 33;
+
+    printf("Antes de la función: %d\n", i);
+    printf( "content of p1: %p\n", ( void * )(&i));
+
+    ft_ft(&i);
+
+    printf("Después de la función: %d\n", i);
+    return 0;
+}
